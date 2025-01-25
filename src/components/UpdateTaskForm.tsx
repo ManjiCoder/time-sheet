18725d51411d24a979892d9c2410bacb1b5a6e5e
Manji Coder <manjicoder@gmail.com>
2025-01-25 16:48:24 +0530

    update Form

--- src/components/UpdateTaskForm.tsx
+++ src/components/UpdateTaskForm.tsx
@@ -15,10 +15,19 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Checkbox } from './ui/checkbox';
 
 const formSchema = z.object({
+  isActive: z.boolean(),
   startTime: z.string().min(2, {
     message: 'Username must be at least 2 characters.',
+  }),
+  endTime: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  category: z.string().default('Misc'),
+  duration: z.string().min(1, {
+    message: 'Username must be at least 1 characters.',
   }),
 });
 
@@ -27,7 +36,11 @@ export function UpdateTaskForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      isActive: false,
       startTime: '',
+      endTime: '',
+      category: '',
+      duration: '',
     },
   });
 
@@ -40,7 +53,10 @@ export function UpdateTaskForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='grid my-8 grid-cols-2 items-center justify-center  gap-5'
+      >
         <FormField
           control={form.control}
           name='startTime'
@@ -48,16 +64,88 @@ export function UpdateTaskForm() {
             <FormItem>
               <FormLabel>Start Time</FormLabel>
               <FormControl>
-                <Input type='date' placeholder='shadcn' {...field} />
+                <Input type='date' placeholder='dd-mm-yyyy' {...field} />
               </FormControl>
               <FormDescription>
-                This is your public display name.
+                {/* This is your public display name. */}
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type='submit'>Submit</Button>
+        <FormField
+          control={form.control}
+          name='endTime'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>End Time</FormLabel>
+              <FormControl>
+                <Input type='date' placeholder='dd-mm-yyyy' {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* This is your public display name. */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='category'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <FormControl>
+                <Input type='text' placeholder='Category' {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* This is your public display name. */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='duration'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Duration</FormLabel>
+              <FormControl>
+                <Input type='text' placeholder='Duration' {...field} />
+              </FormControl>
+              <FormDescription>
+                {/* This is your public display name. */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name='isActive'
+          render={({ field }) => (
+            <FormItem className='col-span-2 space-x-3 flex items-center justify-center'>
+              <FormControl>
+                <Checkbox />
+              </FormControl>
+              <FormLabel>Mark as Compeleted</FormLabel>
+              <FormDescription>
+                {/* This is your public display name. */}
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <section className='my-8 flex justify-center space-x-8 items-center col-span-2'>
+          <Button type='submit'>Submit</Button>
+          <Button type='button' variant='secondary'>
+            Cancel
+          </Button>
+        </section>
       </form>
     </Form>
   );
